df5e26579da0af56116c93b5afdf6d6a781242b4
mohamed baraka <barakamohamed946@gmail.com>
2025-09-14 16:16:21 +0300

    add 'list' command

--- index.js
+++ index.js
@@ -2,7 +2,20 @@
 const { Command } = require("commander");
 const program = new Command();
 
-const expenses = [];
+const expenses = [
+  {
+    id: 1,
+    date: new Date().toDateString(),
+    description: "Groceries",
+    amount: 50,
+  },
+  {
+    id: 2,
+    date: new Date().toDateString(),
+    description: "Entertainment",
+    amount: 100,
+  },
+];
 
 program
   .command("add")
@@ -17,8 +30,19 @@ program
       amount: options.amount,
     };
     expenses.push(expense);
+    console.log(`Expense added successfully (ID: ${expense.id})`);
+  });
+
+program
+  .command("list")
+  .description("List all expenses")
+  .action(() => {
+    console.log(`ID\tDate\t\tDescription\tAmount`);
+    expenses.forEach((expense) =>
+      console.log(
+        `${expense.id}\t${expense.date}\t${expense.description}\t$${expense.amount}`
+      )
+    );
   });
 
 program.parse();
-
-console.log(expenses);
