e1d0a394e2e2f2b493fb0e85d336049b2d369341
mohamed baraka <barakamohamed946@gmail.com>
2025-09-14 16:01:57 +0300

    add 'add' command

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 #!/usr/bin/env node
+const { Command } = require("commander");
+const program = new Command();
+
+const expenses = [];
+
+program
+  .command("add")
+  .description("Add an expense with an amount and description")
+  .option("-d, --description <description>", "Expense description")
+  .option("-a --amount <amount>", "Expense amount")
+  .action((options) => {
+    const expense = {
+      id: expenses.length + 1,
+      date: new Date().toDateString(),
+      description: options.description,
+      amount: options.amount,
+    };
+    expenses.push(expense);
+  });
+
+program.parse();
+
+console.log(expenses);
