fae50f008e238c3d46d84e901483b7bb454ad173
mohamed baraka <barakamohamed946@gmail.com>
2025-09-17 14:12:19 +0300

    add summary command

--- index.js
+++ index.js
@@ -38,7 +38,46 @@ program
   .action(async (options) => {
     const { id } = options;
     await deleteFromFile(id);
-    console.log(`Expense with ID ${id} deleted successfully`);
+    console.log(`Expense deleted successfully`);
+  });
+
+// Summary command
+program
+  .command("summary")
+  .description("Display a summary of expenses")
+  .option("-m, --month <month>", "Filter by month")
+  .action(async (options) => {
+    const expenses = await readFromFile();
+    let totalAmount = 0;
+    let monthMap = new Map([
+      [1, "Jan"],
+      [2, "Feb"],
+      [3, "Mar"],
+      [4, "Apr"],
+      [5, "May"],
+      [6, "Jun"],
+      [7, "Jul"],
+      [8, "Aug"],
+      [9, "Sep"],
+      [10, "Oct"],
+      [11, "Nov"],
+      [12, "Dec"],
+    ]);
+    if (options.month) {
+      let commandMonth = monthMap.get(+options.month);
+      const filteredExpenses = expenses.filter((expense) => {
+        const expenseMonth = expense.date.split(" ")[1];
+        return expenseMonth === commandMonth;
+      });
+      totalAmount = filteredExpenses.reduce(
+        (acc, expense) => acc + +expense.amount,
+        0
+      );
+      console.log(`Total Expenses for ${commandMonth}: $${totalAmount}`);
+    } else {
+      totalAmount = expenses.reduce((acc, expense) => acc + +expense.amount, 0);
+      console.log(`Total Expenses: $${totalAmount}`);
+    }
   });
 
 program.parse();
